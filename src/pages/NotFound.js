3ffd541b1dd9d1ff019733437b2d3fd39cd1426a
mlanorero <mlanorero@gmail.com>
2020-05-29 14:00:53 -0400

    Search Movie ready

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { ButtonBackToHome } from '../components/ButtonBackToHome'
+
+export const NotFound = () => (
+  <div>
+    <h1 className='title'>404!</h1>
+    <h2 className='subtitle'>No existe la página</h2>
+    <ButtonBackToHome />
+  </div>
+
+
+)
